4329d63a94e2eaa6fa28bfa357bec386db3cb0c3
VictorSMQuirino <victorsouzaq@gmail.com>
2022-12-07 21:44:57 -0300

    Correção na validação do campo url dos vídeos

--- src/main/java/challenge/alura/api/video/DadosCadastroVideos.java
+++ src/main/java/challenge/alura/api/video/DadosCadastroVideos.java
@@ -1,12 +1,13 @@
 package challenge.alura.api.video;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record DadosCadastroVideos(
         @NotBlank
         String titulo,
         @NotBlank
         String descricao,
-        @NotBlank
+        @NotBlank @URL(protocol = "http")
         String url) {
 }
