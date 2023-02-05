83dba49247858b1c0d1694eac98106b07ba02ea8
VictorSMQuirino <victorsouzaq@gmail.com>
2023-02-05 13:07:49 -0300

    Alteração de nome de método

--- src/test/java/challenge/alura/api/ApiApplicationTests.java
+++ src/test/java/challenge/alura/api/ApiApplicationTests.java
@@ -1,13 +1,25 @@
 package challenge.alura.api;
 
+import challenge.alura.api.categoria.Categoria;
+import challenge.alura.api.categoria.CategoriaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void cadastroDeCategoria(){
+		CategoriaRepository repository;
+		Categoria categoria = new Categoria(1L, "categoria x", "Preto");
+
+		//assertThrows(repository.save(categoria), categoria.getClass());
 	}
 
 }
